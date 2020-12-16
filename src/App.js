e8093f603f072ab540c0fbe404c831b421a817bf
Pierre Beaufils <pierre.beaufils@outlook.fr>
2020-12-16 19:07:41 +0100

    intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Intro from './components/Intro';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro/index.js
@@ -0,0 +1,20 @@
+import './intro.scss';
+import illustrationIntro from '../../images/illustration-intro.png';
+import curvyDesktop from '../../images/bg-curvy-desktop.svg';
+import curvyMobile from '../../images/bg-curvy-mobile.svg';
+
+function Intro() {
+  return (
+    <div className="intro-container">
+      <div className="intro">
+        <img src={illustrationIntro} alt="logo" />
+        <h2>All your files in one secure location, accessible anywhere.</h2>
+        <p>Fylo stores all your most important files in one secure location.</p>
+        <p>Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
+        <button className="startedButton">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
